1f34f6097a92c8b12e6685df01a0d52ec5444cd3
paul-simon emechebe <paul.emechebe@gmail.com>
2021-07-20 18:12:04 +0100

    first commit

---
+++ public/forms/contact.php
@@ -0,0 +1,78 @@
+
+<?php
+ 
+
+  // Replace contact@example.com with your real receiving email address
+  // $receiving_email_address = 'paul.emechebe@gmail.com';
+
+  if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
+    include( $php_email_form );
+  } else {
+    die( 'Unable to load the "PHP Email Form" Library!');
+  }
+
+  $contact = new PHP_Email_Form;
+  $contact->ajax = true;
+  
+  // $contact->to = $receiving_email_address;
+  // $contact->from_name = $_POST['name'];
+  // $contact->from_email = $_POST['email'];
+  // $contact->subject = $_POST['subject'];
+
+  // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
+  
+  // $contact->smtp = array(
+  //   'host' => 'smtp.gmail.com',
+  //   'username' => 'paul.emechebe@gmail.com',
+  //   'password' => 'emechebepaulsimon',
+  //   'port' => '587'
+  // );
+  
+
+  // $contact->add_message( $_POST['name'], 'From');
+  // $contact->add_message( $_POST['email'], 'Email');
+  // $contact->add_message( $_POST['message'], 'Message', 10);
+
+  // echo $contact->send();
+  use PHPMailer\PHPMailer\PHPMailer;
+
+require_once 'phpmailer/Exception.php';
+require_once 'phpmailer/PHPMailer.php';
+require_once 'phpmailer/SMTP.php';
+
+$mail = new PHPMailer(true);
+
+$alert = '';
+
+if(isset($_POST['submit'])){
+  $name = $_POST['name'];
+  $email = $_POST['email'];
+  $message = $_POST['message'];
+
+  try{
+    $mail->isSMTP();
+    $mail->Host = 'smtp.gmail.com';
+    $mail->SMTPAuth = true;
+    $mail->Username = 'paul.emechebe@gmail.com'; // Gmail address which you want to use as SMTP server
+    $mail->Password = 'emechebepaulsimon'; // Gmail address Password
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+    $mail->Port = '587';
+
+    $mail->setFrom('paul.emechebe@gmail.com'); // Gmail address which you used as SMTP server
+    $mail->addAddress('paul.emechebe@gmail.com'); // Email address where you want to receive emails (you can use any of your gmail address including the gmail address which you used as SMTP server)
+
+    $mail->isHTML(true);
+    $mail->Subject = 'Message Received (Contact Page)';
+    $mail->Body = "<h3>Name : $name <br>Email: $email <br>Message : $message</h3>";
+
+    $mail->send();
+    $alert = '<div class="alert-success">
+                 <span>Message Sent! Thank you for contacting us.</span>
+                </div>';
+  } catch (Exception $e){
+    $alert = '<div class="alert-error">
+                <span>'.$e->getMessage().'</span>
+              </div>';
+  }
+}
+?>
